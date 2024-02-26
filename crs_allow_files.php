c47ee88b484d16993c2512e396ee4869f314c990
Sapera-SH <142575046+Sapera-SH@users.noreply.github.com>
2024-02-26 10:00:31 +0100

    Add files via upload

---
+++ crs_allow_files.php
@@ -0,0 +1,11 @@
+<?php
+	/*
+		Plugin Name: Cloud Retail Systems A/S - Allow all files
+		Plugin URI: http://cloudretailsystems.dk
+		Description: Tillad upload af alle filer
+		Author: Cloud Retail Systems A/S - Søren Højby
+		Version: 1.0
+		Author URI: http://cloudretailsystems.dk
+		*/
+
+	define('ALLOW_UNFILTERED_UPLOADS', true);
